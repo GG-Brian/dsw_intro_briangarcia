25c45c0ddec2bd94def7f187d5a3e62079276301
GG-Brian <71889035+GG-Brian@users.noreply.github.com>
2022-09-14 12:04:15 +0100

    fecha y hora en PHP
    realiza el pedido de la fecha y hora actual del equipo y la estructura en una tabla simple

---
+++ tabla.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <th>Fecha y horas en PHP por Brian García Gómez</th>
+            </tr>
+            <tr>
+                <td><?php echo strftime("%A %e %B %G"); ?></td>
+                <td><?php echo date("h:i:sa"); ?></td>
+            </tr>
+        </table>
+    </body>
+</html>
